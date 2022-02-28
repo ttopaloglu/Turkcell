3782d4a0a831d008258ff096dbf02637d71d0a4c
tumray <tumra@DESKTOP-5B078SK>
2022-02-28 16:21:07 +0300

    güncell

---
+++ HW1-SolidExample/Kullanıcı.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public abstract class Kullanıcı
+{
+    public string KullaniciAdi { get; set; }
+    public string Sifre { get; }
+    public string Cinsiyet { get;}
+
+}
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
